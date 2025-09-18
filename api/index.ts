bbe0aaacb46bf07a131472ca69849c30447bfb96
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-18 15:08:31 -0400

    more naming

--- api/index.ts
+++ api/index.ts
@@ -18,12 +18,12 @@ const GroupLayer = HttpApiBuilder.group(Api, "group", (handlers) =>
 	handlers.handle("get", () => Effect.succeed("Hello, world!")),
 );
 
-const MyApiLayer = HttpApiBuilder.api(Api).pipe(Layer.provide(GroupLayer));
+const ApiLayer = HttpApiBuilder.api(Api).pipe(Layer.provide(GroupLayer));
 
-const ScalarLayer = HttpApiScalar.layer().pipe(Layer.provide(MyApiLayer));
+const ScalarLayer = HttpApiScalar.layer().pipe(Layer.provide(ApiLayer));
 
 const MainLayer = Layer.mergeAll(
-	MyApiLayer,
+	ApiLayer,
 	ScalarLayer,
 	HttpServer.layerContext,
 );
