e268a74302e3839f7a673eaba15c64603fb06abf
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-18 15:14:31 -0400

    test http layer router

--- api/index.ts
+++ api/index.ts
@@ -4,34 +4,34 @@ import {
 	HttpApiEndpoint,
 	HttpApiGroup,
 	HttpApiScalar,
+	HttpLayerRouter,
 	HttpServer,
 } from "@effect/platform";
 import { Effect, Layer, Schema } from "effect";
 
-const Api = HttpApi.make("myApi").add(
-	HttpApiGroup.make("group")
-		.add(HttpApiEndpoint.get("get", "/").addSuccess(Schema.String))
-		.prefix("/api"),
+const Group = HttpApiGroup.make("group").add(
+	HttpApiEndpoint.get("get", "/").addSuccess(Schema.String),
 );
+
+const Api = HttpApi.make("myApi").add(Group).prefix("/api");
 
 const GroupLayer = HttpApiBuilder.group(Api, "group", (handlers) =>
 	handlers.handle("get", () => Effect.succeed("Hello, world!")),
 );
 
-const ApiLayer = HttpApiBuilder.api(Api).pipe(Layer.provide(GroupLayer));
+const ApiRoutesLayer = HttpLayerRouter.addHttpApi(Api, {
+	openapiPath: "/api/docs/openapi.json",
+}).pipe(Layer.provide(GroupLayer), Layer.provide(HttpServer.layerContext));
 
-const ScalarLayer = HttpApiScalar.layer({
+const DocsRouteLayer = HttpApiScalar.layerHttpLayerRouter({
+	api: Api,
 	path: "/api/docs",
-}).pipe(Layer.provide(ApiLayer));
+});
 
-const MainLayer = Layer.mergeAll(
-	ApiLayer,
-	ScalarLayer,
-	HttpServer.layerContext,
-);
+const MainLayer = Layer.mergeAll(ApiRoutesLayer, DocsRouteLayer);
 
 // Convert the API to a web handler
-const { dispose, handler } = HttpApiBuilder.toWebHandler(MainLayer);
+const { dispose, handler } = HttpLayerRouter.toWebHandler(MainLayer);
 
 process.on("SIGTERM", () => {
 	dispose();
