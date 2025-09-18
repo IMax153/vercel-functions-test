ed40a37edb84236fb8b3427a5aac5ad65241a154
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-18 14:45:15 -0400

    add effect

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,40 @@
+import {
+	HttpApi,
+	HttpApiBuilder,
+	HttpApiEndpoint,
+	HttpApiGroup,
+	HttpApiScalar,
+	HttpServer,
+} from "@effect/platform";
+import { Effect, Layer, Schema } from "effect";
+
+const Api = HttpApi.make("myApi").add(
+	HttpApiGroup.make("group").add(
+		HttpApiEndpoint.get("get", "/").addSuccess(Schema.String),
+	),
+);
+
+const groupLive = HttpApiBuilder.group(Api, "group", (handlers) =>
+	handlers.handle("get", () => Effect.succeed("Hello, world!")),
+);
+
+const MyApiLive = HttpApiBuilder.api(Api).pipe(Layer.provide(groupLive));
+
+const ScalarLayer = HttpApiScalar.layer().pipe(Layer.provide(MyApiLive));
+
+const MainLayer = Layer.mergeAll(
+	MyApiLive,
+	ScalarLayer,
+	HttpServer.layerContext,
+);
+
+// Convert the API to a web handler
+const { dispose, handler } = HttpApiBuilder.toWebHandler(MainLayer);
+
+process.on("SIGTERM", () => {
+	dispose();
+});
+
 export default {
-	fetch(request: Request) {
-		return new Response("Hello from Vercel!");
-	},
+	fetch: handler,
 };
